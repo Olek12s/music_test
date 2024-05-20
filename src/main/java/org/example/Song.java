6659c166e60a68b00971e350be098b7dee812f1e
Olek12s <olekignaczak78@gmail.com>
2024-05-20 13:47:25 +0200

    Initial commit

---
+++ src/main/java/org/example/Song.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public record Song(int ID, String artist, String title, int time)
+{
+
+}
---
+++ src/test/java/org/example/PlaylistTest.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlaylistTest
+{
+    @Test
+    public void isEmptyNewPlaylist()
+    {
+        Playlist pl = new Playlist();
+        assertTrue(pl.isEmpty());
+    }
+    @Test
+    public void isSizeOneAfterAddingFirstElement()
+    {
+        Playlist pl = new Playlist();
+        pl.add(new Song(50, "Krawczyk", "Mój Przyjacielu", 700));
+        assertTrue(pl.size() == 1);
+    }
+
+    @Test
+    public void testIfAddedSongIsTheSame()
+    {
+        Playlist pl = new Playlist();
+        Song s = new Song(50, "Krawczyk", "Mój Przyjacielu", 700);
+        pl.add(s);
+        assertEquals(s, pl.get(0));
+    }
+
+    
+
+}
